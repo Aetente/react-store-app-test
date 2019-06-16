c12ea3c9062d78cb38284d4ab517c5b79559d0c6
Aetente <aetente@gmail.com>
2019-06-16 12:16:58 +0300

    beging components

---
+++ src/components/Details.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Details extends Component {
+    render() {
+        return (
+            <div>
+                Yes I am details yes
+            </div>
+        );
+    }
+}
+
+export default Details;
---
+++ src/components/Product.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Product extends Component {
+    render() {
+        return (
+            <div>
+                <div> I am a product yes</div>
+            </div>
+        );
+    }
+}
+
+export default Product;
